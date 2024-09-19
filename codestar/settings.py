80b9df769aa519738307a39cafa3a0831540405b
Szymon Czapiewski <simonczedu@gmail.com>
2024-09-19 13:48:51 +0000

    Fix SECRET_KEY value

--- codestar/settings.py
+++ codestar/settings.py
@@ -26,9 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'django-insecure-k3%m#9#*dxx2c*)fq^%5pc0@kek4s97f_07e4bhxtt*aig-$u8'
 
-SCRET_KEY = {
-    'default': dj_database_url.parse(os.environ.get("SECRET_KEY"))
-}
+SCRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
