a0c9d07ca40aa8008ed82595ea5b0e7189f62ae4
Szymon Czapiewski <simonczedu@gmail.com>
2024-09-27 11:17:14 +0000

    Create models and views

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import post
+from .models import Post, Comment
 
 # Register your models here.
-admin.site.register(post)
+admin.site.register(Post)
+admin.site.register(Comment)
--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = True
 DEBUG = False
+
+
 ALLOWED_HOSTS = ['8000-simonczedu-djangoblogci-4ilshox0fmj.ws.codeinstitute-ide.net', '.herokuapp.com']
 
 
